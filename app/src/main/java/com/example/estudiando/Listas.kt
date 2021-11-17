7643392a30bb816114eb5c52f81d53ffaed29b04
Jesufertez <maria.fernandez.cortez@gmail.com>
2021-11-17 12:24:14 -0300

    Ejercicio Martes 16 Noviembre

---
+++ app/src/main/java/com/example/estudiando/Listas.kt
@@ -0,0 +1,12 @@
+package com.example.estudiando
+
+open class Listas {
+
+    val numbersList = listOf("one","five","two","three", "four")
+
+    val instanceList = listOf(null, 1, "two",3.0, "four")
+
+    val numbersMap= mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 4, "key11" to 11)
+
+    val unsortedList = listOf(7,8,5,3,2,4)
+}
